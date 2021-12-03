c5d6377d472592797df17217b5559dac2b64f723
mohammad yaasir cheekoory <yaasir1997@gmail.com>
2021-12-04 02:04:54 +0400

    added the layout aonly js needed

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
-const results = document.querySelector("#results");
-
 fetch("https://teclead.de/recruiting/radios")
   .then(response => response.json())
   .then((data) => {
-      console.log(data);
-   console.log(data.radios[0].name);
+      const r1 = document.querySelector('#radio1');
+      const r2 = document.querySelector('#radio2');
+      const r3 = document.querySelector('#radio3');
+      const r4 = document.querySelector('#radio4');
+
+      const r1Channel = document.querySelector("#radioChannel1");
+      const r2Channel = document.querySelector("#radioChannel2");
+      const r3Channel = document.querySelector("#radioChannel3");
+      const r4Channel = document.querySelector("#radioChannel4");
+
+      const img1 = document.querySelector("#img")
+
+    const body = document.body
+    r1.innerText = data.radios[0].name
+    r1Channel.innerText = data.radios[0].frequency
+    img1.innerHTML = `<img src="${data.radios[0]}.image">`
+
+    r2.innerText = data.radios[1].name
+    r2Channel.innerText = data.radios[1].frequency
+
+    r3.innerText = data.radios[2].name
+    r3Channel.innerText = data.radios[2].frequency
+
+    r4.innerText = data.radios[3].name
+    r4Channel.innerText = data.radios[3].frequency
   });
